d24b591aca6503404b85a213c27b1ea7ca5133fc
min-38 <mindino03@gmail.com>
2023-06-22 23:12:53 +0900

    Button 동작 기능 구현

--- WeatherViewController.swift
+++ WeatherViewController.swift
@@ -8,22 +8,29 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    let cities = ["Seoul", "Tokyo", "New York", "London"]
+    let weathers = ["cloud.fill", "sun.max.fill", "wind", "cloud.sun.rain.fill"]
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func changeButtonTapped(_ sender: Any) {
+        cityLabel.text = cities.randomElement()
+        
+        let imageName = weathers.randomElement()!
+        weatherImageView.image = UIImage(systemName: imageName)?.withRenderingMode(.alwaysOriginal)
+    
+        let randomTemp = Int.random(in: 10..<30)
+        temperatureLabel.text = "\(randomTemp)°"
+        print("도시, 온도, 날씨 이미지 변경하자!!")
     }
-    */
 
 }
